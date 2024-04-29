6778d1724acca84290bfbeb74be8cb98a647e9a2
iccee0 <git@roeh.ch>
2024-04-29 13:20:33 +0000

    Get Projects

--- routes/api/joke.ts
+++ routes/api/joke.ts
@@ -5,6 +5,8 @@ const kv = await Deno.openKv();
 export const handler = async (_req: Request, _ctx: FreshContext): Response => {
   const a = []
   for await (const entry of kv.list({ prefix: ["roehh", "projects"] })) {
+    console.log(entry.value, 'entry');
+    
     a.push(entry.value);
   }
   console.log(a, 'a');
